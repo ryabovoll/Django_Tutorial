63e40c98b15c6604b6e3b7f42326fa97202710f9
Aleksandr Ryabovol <ryabovol.97@gmail.com>
2022-11-16 16:12:12 +0200

    df

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,6 @@ from django.db import models
 from django.utils import timezone
 
 
-from django.conf import settings
-from django.db import models
-from django.utils import timezone
-
-
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -21,4 +16,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
